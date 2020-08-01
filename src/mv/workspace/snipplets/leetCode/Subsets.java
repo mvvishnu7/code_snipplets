58df16161a8331a4eb55cd759bb93a80d3f4af61
vishnu.mv <vishnu.mv@auto1.com>
2020-08-01 13:29:13 +0200

    leetcode-78-Subsets

--- src/mv/workspace/snipplets/leetCode/Subsets.java
+++ src/mv/workspace/snipplets/leetCode/Subsets.java
@@ -1,7 +1,30 @@
 package mv.workspace.snipplets.leetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/subsets/
  */
 public class Subsets {
+
+    public static void main(String[] args) {
+        List<List<Integer>> subsets = new Subsets().subsets(new int[]{1, 2, 3});
+        System.out.println(subsets.toString());
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(result, nums, 0, new ArrayList<>());
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, int[] nums, int index, List<Integer> current) {
+        result.add(new ArrayList<>(current));
+        for(int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            dfs(result, nums, i + 1, current);
+            current.remove(current.size() - 1);
+        }
+    }
 }
